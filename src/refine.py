a7a5fa139495af3de65b9bc6c6efae4d7da1ac80
ufgtb24 <1026773987@qq.com>
2019-04-25 11:03:16 +0800

    添加 valid

--- src/refine.py
+++ src/refine.py
@@ -46,6 +46,11 @@ train_data_path = 'F:/ProjectData/surface/leg'
 ckpt_path = 'F:/tf_projects/3D/FeaStNet-master/ckpt'
 x, adj, pidx, pedge, y, ynm, mask = \
     get_training_data(train_data_path, load_previous=True)
+
+valid_data_path = 'F:/ProjectData/surface/leg'
+x_v, adj_v, pidx_v, pedge_v, y_v, ynm_v, mask_v = \
+    get_training_data(valid_data_path, load_previous=True)
+
 # x, adj, pidx,pedge, y, ynm,mask=get_training_data(annotation_path)
 
 # dir_load = '20190418-1138'  # where to restore the model
@@ -69,7 +74,17 @@ with tf.Session(config=config)as sess:
     
     epochs = 100000
     min_loss = 1000000
-    
+    avg_loss={'loss':0,'Chamfer_loss':0,'edge_loss':0,'normal_loss':0,'lap_loss':0}
+    avg_loss_v={'loss':0,'Chamfer_loss':0,'edge_loss':0,'normal_loss':0,'lap_loss':0}
+    epc_loss={}
+    def sum_epoch_loss(avg_loss,epoch_loss,epoch_i):
+        avg_loss['loss'] = (avg_loss['loss'] * epoch_i + epoch_loss['loss']) / (epoch_i + 1)
+        avg_loss['Chamfer_loss'] = (avg_loss['Chamfer_loss'] * epoch_i + epoch_loss['Chamfer_loss']) / (epoch_i + 1)
+        avg_loss['edge_loss'] = (avg_loss['edge_loss'] * epoch_i + epoch_loss['edge_loss']) / (epoch_i + 1)
+        avg_loss['normal_loss'] = (avg_loss['normal_loss'] * epoch_i + epoch_loss['normal_loss']) / (epoch_i + 1)
+        avg_loss['lap_loss'] = (avg_loss['lap_loss'] * epoch_i + epoch_loss['lap_loss']) / (epoch_i + 1)
+
+
     for epoch in range(epochs):
         for x_, adj_, pidx_, pedge_, y_, ynm_, mask_ in zip(x, adj, pidx, pedge, y, ynm, mask):
             feed_in = {X: x_,
@@ -81,20 +96,47 @@ with tf.Session(config=config)as sess:
                        Mask: mask_,
                        }
             
-            _, loss, Chamfer_loss1, edge_loss1, normal_loss1, lap_loss1, \
-                = sess.run([optimizer,
-                            total_loss, Chamfer_loss, edge_loss, normal_loss, lap_loss,
-                
-                            ],
+            _, epc_loss['loss'], epc_loss['Chamfer_loss'], epc_loss['edge_loss'], epc_loss['normal_loss'], epc_loss['lap_loss'], \
+                = sess.run([optimizer,total_loss, Chamfer_loss, edge_loss, normal_loss, lap_loss],
                            feed_dict=feed_in)
+            sum_epoch_loss(avg_loss,epc_loss,epoch)
+            
+        
+        
+        for x_, adj_, pidx_, pedge_, y_, ynm_, mask_ in zip(x_v, adj_v, pidx_v, pedge_v, y_v, ynm_v, mask_v):
+            feed_in = {X: x_,
+                       Adj: adj_,
+                       Pidx: pidx_,
+                       Pedge: pedge_,
+                       Y: y_,
+                       Ynm: ynm_,
+                       Mask: mask_,
+                       }
+            epc_loss['loss'], epc_loss['Chamfer_loss'], epc_loss['edge_loss'], epc_loss['normal_loss'], epc_loss[
+                'lap_loss'], \
+                = sess.run([total_loss, Chamfer_loss, edge_loss, normal_loss, lap_loss],
+                           feed_dict=feed_in)
+            sum_epoch_loss(avg_loss_v,epc_loss,epoch)
+
+            
+
         if epoch % 1 == 0:
-            if loss < min_loss:
-                min_loss = loss
+            if avg_loss_v['loss'] < min_loss:
+                min_loss = avg_loss_v['loss']
                 print('save ckpt\n')
                 saver.save(sess, save_checkpoints_dir + "/model.ckpt", global_step=int(epoch))
-            
+            if epoch % 30 == 0:
+                print('save ckpt\n')
+                saver.save(sess, save_checkpoints_dir + "/model.ckpt", global_step=int(epoch))
+
+            print('TRAIN_SUMMARY:\n')
             print('epoch = %d \nloss=%.4f\nChamfer_loss=%.4f\nedge_loss=%.4f\nnormal_loss=%.4f\nlap_loss=%.4f\n'
-                  % (epoch, loss, Chamfer_loss1, edge_loss1, normal_loss1, lap_loss1))
+                  % (epoch, avg_loss['loss'], avg_loss['Chamfer_loss'],
+                     avg_loss['edge_loss'], avg_loss['normal_loss'], avg_loss['lap_loss']))
+            print('VALID_RESULT:\n')
+            print('epoch = %d \nloss=%.4f\nChamfer_loss=%.4f\nedge_loss=%.4f\nnormal_loss=%.4f\nlap_loss=%.4f\n'
+                  % (epoch, avg_loss_v['loss'], avg_loss_v['Chamfer_loss'],
+                     avg_loss_v['edge_loss'], avg_loss_v['normal_loss'], avg_loss_v['lap_loss']))
             
             print('===============')
 
