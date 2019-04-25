9fefdbf9697ec938406343e7f6e3a62630b6dbe0
ufgtb24 <1026773987@qq.com>
2019-04-25 18:13:23 +0800

    集成 subdivide 到 get_training_data; 接下来需要根据两个 x x' face face' 推测 P_face_idx

--- src/data_process.py
+++ src/data_process.py
@@ -13,14 +13,14 @@ def get_training_data(root_path, load_previous=True):
                data['Y'], data['Ynm'], data['Mask']
     
     sample_dir = os.listdir(root_path)
-    x_list = []
-    face_list=[]
-    adj_list = []
-    p_idx_list = []
-    p_edge_list = []
+    cas=3
+    x_list = [[]]*cas
+    adj_list = [[]]*cas
+    p_idx_list = [[]]*cas
+    p_edge_list = [[]]*cas
+    mask_list = [[]]*cas
     y_list = []
     y_nm_list = []
-    mask_list = []
     
     for sample in sample_dir:
         sample_path = os.path.join(root_path, sample)
@@ -29,44 +29,45 @@ def get_training_data(root_path, load_previous=True):
             face = np.loadtxt(os.path.join(sample_path, 'face.txt'))[:, 1:].astype(np.float32)
             face_idx = np.loadtxt(os.path.join(sample_path, 'face_idx.txt')).astype(np.int32)
             p_idx = np.loadtxt(os.path.join(sample_path, 'x_add_idx.txt')).astype(np.int32)
-
-            p_edge, adj = extract_face(face, p_idx)
-
-            mask = build_mask(x.shape[0], p_idx)
-
-            new_faces, new_face_index, new_pt,mar_pt_id, mar_edges\
-                =subdivide(x.shape[0],face,face_idx)
-            
-            
-            
-            
-            
             y_nm = np.loadtxt(os.path.join(sample_path, 'y_normal.txt')).astype(np.float32)
             y = y_nm[:, :3]
             y_nm = y_nm[:, 3:]
             
-            x_list.append(x)
-            face_list.append(face)
-            adj_list.append(adj)
-            p_idx_list.append(p_idx)
-            p_edge_list.append(p_edge)
-            y_list.append(y)
-            y_nm_list.append(y_nm)
-            mask_list.append(mask)
+            for i,c in enumerate(range(cas)):
+                mar_pt=None
+                mar_nb=None
+                vertice_num=x.shape[0]
+                if i>0:
+                    
+                    face, face_idx, new_pt_nb,mar_pt, mar_nb\
+                        =subdivide(vertice_num,face,face_idx)
+                    
+                    new_pt_num=new_pt_nb.shape[0]
+                    p_idx=np.vstack((p_idx,vertice_num+np.arange(new_pt_num)))
+                    vertice_num+=new_pt_num
+                
+                
+                p_edge, adj, face = extract_face(face, p_idx,mar_pt,mar_nb)
+                mask = build_mask(vertice_num, p_idx)
+
+                
+                
+                
+                x_list[c].append(x)
+                adj_list[c].append(adj)
+                p_idx_list[c].append(p_idx)
+                p_edge_list[c].append(p_edge)
+                mask_list[c].append(mask)
+                y_list.append(y)
+                y_nm_list.append(y_nm)
     
     X = np.array(x_list)
-    Face = np.array(face_list)
     Adj = np.array(adj_list)
     Pidx = np.array(p_idx_list)
     Pedge = np.array(p_edge_list)
     Y = np.array(y_list)
     Ynm = np.array(y_nm_list)
     Mask = np.array(mask_list)
-    ################
-    
-    
-    
-    ################
     
     if not load_previous:
         np.savez(load_path, X=X, Adj=Adj, Pidx=Pidx, Pedge=Pedge,
@@ -74,7 +75,6 @@ def get_training_data(root_path, load_previous=True):
     
     return X, Adj, Pidx, Pedge, Y, Ynm, Mask
 
-    p_edges, adj=extract_face(faces, p_idx)
     
 
 def build_mask(num, x_add_idx):
@@ -93,18 +93,18 @@ def gather_from_one(x, idx):
 
 def subdivide(vertice_num,
               faces,
-              face_index=None):
+              face_idx=None):
 
-    if face_index is None:
-        face_index = np.arange(len(faces))
+    if face_idx is None:
+        face_idx = np.arange(len(faces))
     else:
-        face_index = np.asanyarray(face_index)
+        face_idx = np.asanyarray(face_idx)
 
     # the (c,3) int set of vertex indices
-    faces = faces[face_index]
+    faces = faces[face_idx]
     # f =len(face_index)
     # [3*f,2]  0-f: [01] f-2f[12]  2f-3f[20]
-    mid= np.stack([faces[:,e]for e in [[0, 1],
+    mid= np.vstack([faces[:,e]for e in [[0, 1],
                                [1, 2],
                                [2, 0]]])
 
@@ -114,17 +114,13 @@ def subdivide(vertice_num,
     # [num_edge] [3*f]. unique 的每个值代表一个不重复的新中点在所有中点中的索引
     unique, inverse = grouping.unique_rows(mid)
     idx=np.array([id for id, v in Counter(inverse).items() if v==1])
-    new_pt=mid[unique]
+    new_pt_nb=mid[unique]
     mar_pt=idx+vertice_num # 边缘点的 id [mar_num]
-    mar_neigb=mid[unique[idx]] #[mar_num,2]
-    mar_in_f =unique[idx]//len(face_index)  #[margin_num] kind of edge
-    mar_f_id=unique[idx] %  len(face_index) #[margin_num] f id
-    other_pt_mar={0:2,1:0,2:1}
-    other_class=[other_pt_mar[i] for i in list(mar_in_f)]
-    other_id=faces[mar_f_id,other_class] #[margin_num]
+    mar_nb=mid[unique[idx]] #[mar_num,2]
+    
     # [f,3]  3 column means 3 kinds of middle points [01] [12] [20]
     # which is duplicated in two adjacency triangles
-    mid_idx = (np.arange(len(face_index) * 3)).reshape((3, -1)).T
+    mid_idx = (np.arange(len(face_idx) * 3)).reshape((3, -1)).T
 
 
     # [f,3] 新face关于 新点的索引
@@ -146,11 +142,11 @@ def subdivide(vertice_num,
                          mid_idx[:, 2]]).reshape((-1, 3))
     # add the 3 new faces per old face
     # stack [f,3] [3*f,3] -> [4*f,3]
-    new_faces = np.vstack((faces, small_faces[len(face_index):]))
+    new_face = np.vstack((faces, small_faces[len(face_idx):]))
     # replace the old face with a smaller face
-    new_faces[face_index] = small_faces[:len(face_index)]
-    new_face_index=np.concatenate([face_index,np.arange(len(face_index),4*len(face_index))])
-    return  new_faces,new_face_index,new_pt,mar_pt,mar_neigb,other_id
+    new_face[face_idx] = small_faces[:len(face_idx)]
+    new_face_idx=np.concatenate([face_idx, np.arange(len(face_idx), 4 * len(face_idx))])
+    return  new_face,new_face_idx,new_pt_nb,mar_pt,mar_nb
 
 # def extract_edge(p_idx, p_adj):
 #     '''
@@ -169,7 +165,7 @@ def subdivide(vertice_num,
 #     return pairs
 
 
-def extract_face(faces, p_idx, mar_pt, mar_nb):
+def extract_face(faces, p_idx, mar_pt=None, mar_nb=None):
     '''
     
     :param faces: f,3
@@ -184,42 +180,44 @@ def extract_face(faces, p_idx, mar_pt, mar_nb):
     edges = np.stack([faces[:, g]
                       for g in [[0, 1], [1, 0],
                                 [1, 2]]],axis=1)
+    if mar_pt is not None:
 
-    #[f(x) if condition else g(x) for x in sequence]
-    mar_nb= mar_nb[:, [1, 0]]
-    mar_num=mar_nb.shape[0]
-    # faces=np.expand_dims(faces,axis=2)#[f,3,1,2]
-    # (faces == mar_nb).all(axis=-1)#[f,3,m]
-    
-    mar_nb=np.reshape(mar_nb,[-1,1,1,2]) #[m,1,1,2]
-    mar_e=(edges == mar_nb).all(axis=-1)  # [m,f,3] bool 只有m个值为 True
-    mar_f=mar_e.any(axis=-1)  # [m,f] bool 只有m个值为 True
-    mar_f_ne=np.expand_dims(mar_f,-1)*~mar_e # [m,f,3] bool  有2m个值为 True
-    
-    #[2m] [2m] [2m]
-    n_idx,f_idx,e_idx=np.where(mar_f_ne)
-    mar_ne=edges[f_idx,e_idx,:].reshape([mar_num,2,2]) #[m,2,2]
-    mar_ne1=mar_ne[:,0,:] #[m,2]
-    mar_ne2=mar_ne[:,1,:] #[m,2]
-    mar_ne2=mar_ne2[:,:,[1,0]] #[m,2]
-    mask=np.where((mar_ne1-mar_ne2)==0)[0] #[m]
-    mar_op_pt=mar_ne1[np.arange(mar_num),mask] #[m]
-    
-    
-    # # 只剩下一个face 和一个 margin对应，即洞外face
-    # # 分割出 两个 新平面
-    
-    split_faces1=np.stack([mar_nb[:,0],mar_pt,mar_op_pt],axis=-1) #[m,3]
-    split_faces2=np.stack([mar_nb[:,1],mar_op_pt,mar_pt],axis=-1) #[m,3]
-    split_faces=np.concatenate([split_faces1,split_faces2]) #[2*m,3]
-    faces=np.concatenate([faces,split_faces]) #[f+2m,3]
-    
-    # [2*m,3,2]
-    split_edges = np.stack([split_faces[:, g]
-                      for g in [[0, 1], [1, 0],
-                                [1, 2]]],axis=1)
-    edges=np.concatenate([edges,split_edges]) #[f+2m,3,2]
-    
+        #[f(x) if condition else g(x) for x in sequence]
+        mar_nb= mar_nb[:, [1, 0]]
+        mar_num=mar_nb.shape[0]
+        # faces=np.expand_dims(faces,axis=2)#[f,3,1,2]
+        # (faces == mar_nb).all(axis=-1)#[f,3,m]
+        
+        mar_nb=np.reshape(mar_nb,[-1,1,1,2]) #[m,1,1,2]
+        mar_e=(edges == mar_nb).all(axis=-1)  # [m,f,3] bool 只有m个值为 True
+        mar_f=mar_e.any(axis=-1)  # [m,f] bool 只有m个值为 True
+        mar_f_ne=np.expand_dims(mar_f,-1)*~mar_e # [m,f,3] bool  有2m个值为 True
+        
+        #[2m] [2m] [2m]
+        n_idx,f_idx,e_idx=np.where(mar_f_ne)
+        mar_ne=edges[f_idx,e_idx,:].reshape([mar_num,2,2]) #[m,2,2]
+        mar_ne1=mar_ne[:,0,:] #[m,2]
+        mar_ne2=mar_ne[:,1,:] #[m,2]
+        mar_ne2=mar_ne2[:,:,[1,0]] #[m,2]
+        mask=np.where((mar_ne1-mar_ne2)==0)[0] #[m]
+        mar_op_pt=mar_ne1[np.arange(mar_num),mask] #[m]
+        
+        
+        # # 只剩下一个face 和一个 margin对应，即洞外face
+        # # 分割出 两个 新平面
+        
+        split_faces1=np.stack([mar_nb[:,0],mar_pt,mar_op_pt],axis=-1) #[m,3]
+        split_faces2=np.stack([mar_nb[:,1],mar_op_pt,mar_pt],axis=-1) #[m,3]
+        split_faces=np.concatenate([split_faces1,split_faces2]) #[2*m,3]
+        faces=np.concatenate([faces,split_faces]) #[f+2m,3]
+        
+        # [2*m,3,2]
+        split_edges = np.stack([split_faces[:, g]
+                          for g in [[0, 1], [1, 0],
+                                    [1, 2]]],axis=1)
+        edges=np.concatenate([edges,split_edges]) #[f+2m,3,2]
+
+    edges=edges.reshape([-1,2])
     unique, inverse = grouping.unique_rows(edges)
     edges = edges[unique]
     edges_sym=edges[:,[1,0]]
@@ -229,7 +227,7 @@ def extract_face(faces, p_idx, mar_pt, mar_nb):
     unique, inverse = grouping.unique_rows(edges[:, 0])
     
     p_unique=unique[p_idx-1]
-    p_edges=np.stack([edges[st:ed]
+    p_edges=np.vstack([edges[st:ed]
            for st, ed in zip(p_unique[:-1], p_unique[1:])])
     adj = [np.concatenate([edges[:, 1][st:ed], np.zeros([10 - (ed - st)])])
            for st, ed in zip(unique[:-1], unique[1:])]
@@ -275,10 +273,10 @@ def output(annotation_path):
 
 
 if __name__ == '__main__':
-    train_data_path = 'F:/ProjectData/surface/rabbit/2aitest'
+    train_data_path = 'F:/ProjectData/surface/leg/train'
     
-    x, adj, pidx, pedge, y, ynm, mask = get_training_data(train_data_path,
-                                                          data_path=train_data_path, load_previous=False)
+    x, adj, pidx, pedge, y, ynm, mask = \
+        get_training_data(train_data_path,load_previous=False)
     
     # output(annotation_path)
 
